620efc290ecea152ed25d73cea0d1741432fa7a3
Michael Van Brummen <michaelvan.brummen@siteminder.com>
2023-05-08 14:28:48 +1000

    Added children comments

--- components/Comments/index.tsx
+++ components/Comments/index.tsx
@@ -3,6 +3,7 @@ import {Item} from "../../lib/types/item";
 import {Comments} from "../../lib/types/comments";
 import {convertUnixTimeToSince} from "../../lib/utils/dateUtil";
 import styles from './comments.module.scss';
+import _ from "lodash";
 
 interface Props {
     item: Item,
@@ -11,16 +12,21 @@ interface Props {
 
 const Comments = ({item, comments}: Props) => (
     <div>
-        {comments.map((comment, idx) => <Comment key={idx} comment={comment}/>)}
+        {comments.map((comment, idx) => <Comment key={idx} comment={comment} depth={0}/>)}
     </div>
 );
 
-const Comment = ({comment}: { comment: Comments }) => {
+const Comment = ({comment, depth = 1}: { comment: Comments, depth: number }) => {
     return (
-        <div className={styles.comment}>
+        <div className={styles.comment} style={{marginLeft: depth * 10}}>
             <div className={styles.user}>{comment.item.by}<span> {convertUnixTimeToSince(comment.item.time)} ago</span>
             </div>
             <div>{comment.item.text}</div>
+            <div>
+                {comment.children && !_.isEmpty(comment.children) &&
+                    comment.children.map((childComments, idx) => <Comment key={idx} comment={childComments}
+                                                                          depth={depth + 1}/>)}
+            </div>
         </div>
     )
 }
