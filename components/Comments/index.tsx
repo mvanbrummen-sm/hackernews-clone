4a6cf2dc62d603bc34c68f89254ad9776c53354c
Michael Van Brummen <michaelvan.brummen@siteminder.com>
2023-05-08 15:09:41 +1000

    Added collapsible comments

--- components/Comments/index.tsx
+++ components/Comments/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Item} from "../../lib/types/item";
 import {Comments} from "../../lib/types/comments";
 import {convertUnixTimeToSince} from "../../lib/utils/dateUtil";
@@ -17,15 +17,22 @@ const Comments = ({item, comments}: Props) => (
 );
 
 const Comment = ({comment, depth = 1}: { comment: Comments, depth: number }) => {
+    const [collapsed, setCollapsed] = useState(false)
+
     return (
         <div className={styles.comment} style={{marginLeft: depth * 10}}>
-            <div className={styles.user}>{comment.item.by}<span> {convertUnixTimeToSince(comment.item.time)} ago</span>
+            <div
+                className={styles.user}>{comment.item.by}<span> {convertUnixTimeToSince(comment.item.time)} ago</span> | <a
+                href={'javascript:void(0)'}
+                onClick={() => setCollapsed(!collapsed)}>[{collapsed ? comment.children.length + ' more' : '-'}]</a>
             </div>
-            <div>{comment.item.text}</div>
-            <div>
-                {comment.children && !_.isEmpty(comment.children) &&
-                    comment.children.map((childComments, idx) => <Comment key={idx} comment={childComments}
-                                                                          depth={depth + 1}/>)}
+            <div hidden={collapsed}>
+                <div>{comment.item.text}</div>
+                <div>
+                    {comment.children && !_.isEmpty(comment.children) &&
+                        comment.children.map((childComments, idx) => <Comment key={idx} comment={childComments}
+                                                                              depth={depth + 1}/>)}
+                </div>
             </div>
         </div>
     )
